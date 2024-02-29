53dd978eaa88a723aa486fb23aa9c9340f623f2e
sonicwall <sonicwall@sonicwalls-MacBook-Pro.local>
2024-03-01 00:49:12 +0530

    env virable set

--- applicationmaster.py
+++ applicationmaster.py
@@ -1,10 +1,11 @@
 from dask import dataframe as dd
 import sys
+import os
 def convert_data():
-    df = dd.read_csv('data-master/nyse_all/nyse_data/NYSE_*.txt.gz',
+    df = dd.read_csv(os.environ['SRC_FILE_PATTERN'],
                      names=["ticker","trade_date","open_price","low_price","high_price","close_price","volume"],
                      blocksize=None)
-    df.to_json('data-master/nyse_all/nyse_json/NYSE_*.json.gz',orient='records',lines=True,compression='gzip',name_function=lambda i:'%05d' %i)
+    df.to_json(os.environ['DESTIN_FILE_PATTERN'],orient='records',lines=True,compression='gzip',name_function=lambda i:'%05d' %i)
 
 
 if __name__ == "__main__":
