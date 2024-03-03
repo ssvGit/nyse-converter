016336e8e8f94de26e9f787e0948b1a6fc3d6ab7
sonicwall <sonicwall@sonicwalls-MacBook-Pro.local>
2024-03-03 11:42:48 +0530

    additional changes

--- applicationmaster.py
+++ applicationmaster.py
@@ -1,12 +1,18 @@
 from dask import dataframe as dd
 import sys
 import os
+import glob
+
 def convert_data():
-    df = dd.read_csv(os.environ['SRC_FILE_PATTERN'],
+    src_env =os.environ['SRC_FILE_PATTERN']
+    src_files = glob.glob(f'{src_env}/NYSE*.txt.gz')
+    # trgt_env = os.environ['TGT_FILE_PATTERN']
+    tgt_files = [file.replace('txt','json') for file in src_files]
+    df = dd.read_csv(src_files,
                      names=["ticker","trade_date","open_price","low_price","high_price","close_price","volume"],
                      blocksize=None)
-    df.to_json(os.environ['DESTIN_FILE_PATTERN'],orient='records',lines=True,compression='gzip',name_function=lambda i:'%05d' %i)
-
+    df.to_json(tgt_files,orient='records',lines=True,compression='gzip')
+    
 
 if __name__ == "__main__":
     print(convert_data())
