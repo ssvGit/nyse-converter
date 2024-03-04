890d6ad6fa5ae06247786360a38cf0e380976f03
sonicwall <sonicwall@sonicwalls-MacBook-Pro.local>
2024-03-04 23:28:36 +0530

    adding logging

--- applicationmaster.py
+++ applicationmaster.py
@@ -2,8 +2,10 @@ from dask import dataframe as dd
 import sys
 import os
 import glob
+import logging
 
 def convert_data():
+    logging.info("Starting convert_data")
     src_env =os.environ['SRC_FILE_PATTERN']
     src_files = glob.glob(f'{src_env}/NYSE*.txt.gz')
     # trgt_env = os.environ['TGT_FILE_PATTERN']
@@ -12,7 +14,11 @@ def convert_data():
                      names=["ticker","trade_date","open_price","low_price","high_price","close_price","volume"],
                      blocksize=None)
     df.to_json(tgt_files,orient='records',lines=True,compression='gzip')
+    logging.info("End convert_data")
     
 
 if __name__ == "__main__":
-    print(convert_data())
+    logging.basicConfig(filename="applicationmaster.log",level=logging.INFO,
+                        format='%(levelname)s %(asctime)s %(message)s',
+                        datefmt='%Y-%m-%d %I:%M:%s')
+    logging.info(convert_data())
